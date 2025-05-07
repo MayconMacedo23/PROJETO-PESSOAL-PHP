cd265bd443425717fcc19cb4ce0fe15ae1593fa1
Maycon Macedo <darkyuse@gmail.com>
2025-05-07 14:28:36 -0300

    arrumando design formulario

--- app/views/formulario.php
+++ app/views/formulario.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <title>Nova Tarefa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body>
-    <h1>Cadastrar Tarefa</h1>
+<body class="bg-light">
+    <div class="container mt-5">
+        <div class="card">
+            <div class ="card-header">
+                <h1 class ="h4 mb-0">Cadastrar Tarefa </h1>
+        </div>
+        <div class="card-body">
+
+    </div>
+ 
 
 
     <form action="index.php?action=salvar" method="POST">
@@ -23,7 +32,7 @@
         
         </select><br><br>
 
-        <button type="submit">Salvar</button>
+        <button type="submit" class="btn btn-primary">Salvar</button>
     </form>
 </body>
 </html>
