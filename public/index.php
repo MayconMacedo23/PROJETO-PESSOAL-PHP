e208489a19e9eabe36a1e99f84b5da701b43ba50
Maycon Macedo <darkyuse@gmail.com>
2025-05-07 15:37:14 -0300

    editando index

--- public/index.php
+++ public/index.php
@@ -1,8 +1,15 @@
 <?php
+session_start();
+
+if (isset($_SESSION['mensagem'])) {
+    echo '<div class="alert alert-success text-center">' . $_SESSION['mensagem'] . '</div>';
+    unset($_SESSION['mensagem']);
+}
+
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../app/controllers/TarefaController.php';
 
-$controller = new TarefaController($pdo);
+$controller = new TarefaController(pdo: $pdo);
 
 $action = $_GET['action'] ?? 'index';
 
