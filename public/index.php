7bcd612ec6c8074818eeeac6ffc4fa841eb99410
Maycon Macedo <darkyuse@gmail.com>
2025-05-08 11:39:48 -0300

    ajustando formulario e index ( acho que deu ruim)

--- public/index.php
+++ public/index.php
@@ -2,14 +2,19 @@
 session_start();
 
 if (isset($_SESSION['mensagem'])) {
-    echo '<div class="alert alert-success text-center">' . $_SESSION['mensagem'] . '</div>';
+    echo '
+    <div class="alert alert-success alert-dismissible fade show text-center" role="alert">
+        ' . $_SESSION['mensagem'] . '
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Fechar"></button>
+    </div>';
     unset($_SESSION['mensagem']);
 }
+
 
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../app/controllers/TarefaController.php';
 
-$controller = new TarefaController(pdo: $pdo);
+$controller = new TarefaController($pdo);
 
 $action = $_GET['action'] ?? 'index';
 
